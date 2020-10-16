7fa9058b5f3aedc0ae7b3de77c0b0a25839b0b4f
1754590086@qq.com <1754590086@qq.com>
2020-10-16 17:19:21 +0800

    cal

---
+++ prepare/mycal.c
@@ -0,0 +1,91 @@
+#include <stdio.h>
+
+// 声明
+int is_leap(int y);
+int month_days(int m, int y);
+int main(void)
+{
+	int year, month;
+	int sumdays = 0;
+	int firstweek;
+	int monthdays;
+
+	// 读入 do .... while()
+	while (1) {
+		printf("输入90年后日期(y/m):");
+		scanf("%d/%d", &year, &month);
+		if (year >= 1990 && month >= 1 && month <= 12)
+			break;// 跳出循环
+	}
+	// 统计总天数
+	// 1990~year
+	for (int y = 1990; y < year; y++) {
+		// 调用
+		sumdays += (365+is_leap(y));
+	}
+	// year.1.1~year.month.1
+	for (int m = 1; m < month; m++) {
+		sumdays += month_days(m, year);
+	}
+	sumdays ++;
+
+	// %7
+	firstweek = sumdays % 7;
+	printf("星期%d\n", firstweek);
+
+	// 用户输入的月份有多少天
+	monthdays = month_days(month, year);
+
+	// 打印日历
+	printf("      %d月 %d\n", month, year);
+	printf("日 一 二 三 四 五 六\n");
+	for (int i = 0; i < firstweek; i++)
+		printf("   ");
+	for (int d = 1; d <= monthdays; d++) {
+		printf("%2d", d);	
+		if ((d+firstweek) % 7 == 0)
+			printf("\n");
+		else
+			printf(" ");
+	}
+	printf("\n");
+
+	return 0;
+}
+
+// 函数: 定义:实现 声明 调用
+/*
+is_leap:判断给定的年份是否为润年
+y:待判断的年份
+return:0 no  1 yes
+ */
+int is_leap(int y)
+{
+	if (y % 4 == 0 && y % 100 != 0 || y % 400 == 0)
+		return 1;
+	return 0;
+}
+
+/*
+ 函数功能:计算出给定的月份有多少天
+ */
+int month_days(int m, int y)
+{
+	int sumdays = 0;
+
+	if (m == 1 || m == 3 || m == 5 || m == 7 || m == 8\
+			|| m == 10 || m == 12) {
+		sumdays = 31;
+	} else if (m == 4 || m == 6 || m == 9 || m == 11) {
+		sumdays = 30;
+	} else {
+		sumdays = 28+is_leap(y);
+	}
+
+	return sumdays;
+}
+
+
+
+
+
