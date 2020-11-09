de02635f0b502ad16f0314cc48ee70cbf837cd7c
Your Name <you@example.com>
2020-11-09 17:09:10 +0800

    struct

---
+++ c/day0a/struct.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+
+// 复合类型--->结构体
+struct student_st {
+	char *name;
+	int age;
+	float score;
+};
+
+int main(void)
+{
+	struct student_st s = {"c", 50, 100};
+	struct student_st s2;
+	struct student_st *p;
+	struct student_st arr[3];
+
+	s2 = s;
+	p = &s;
+	printf("%s, %d, %f\n", s2.name, s2.age, s2.score);
+	printf("%s, %d, %f\n", p->name, p->age, p->score);
+
+	return 0;
+}
+
