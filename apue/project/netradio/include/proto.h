d8113173a745a24d69dbcb7d7ad7b3afd4a03ba2
Your Name <you@example.com>
2020-12-22 15:42:37 +0800

    server

--- apue/project/netradio/include/proto.h
+++ apue/project/netradio/include/proto.h
@@ -1,7 +1,7 @@
 #ifndef __NETRADIO_PROTO_H
 #define __NETRADIO_PROTO_H
 
-#pragma pack(1)
+//#pragma pack(1) !!!!!!!!!!!!!!!!!!!!!
 
 #define GROUPADDR	"234.2.3.4"
 #define RCVPORT		1234
@@ -16,22 +16,26 @@
 // 频道列表
 typedef int8_t chnid_t;
 // 每一个频道
-typedef struct {
+struct list_entry_st{
 	chnid_t chnid; // MUST BE > 0
 	int8_t len; // 整个结构字节个数
 	char descr[1];
-}list_entry_t;
+}__attribute__((packed));
+typedef struct list_entry_st list_entry_t;
 
-typedef struct {
+struct chn_list_st{
 	chnid_t chnid; // MUST BE 0
 	list_entry_t entry[1];
-}chn_list_t;
+}__attribute__((packed));
+typedef struct chn_list_st chn_list_t;
+
 
 // 数据
-typedef struct {
+struct chn_data_st{
 	chnid_t chnid;
 	char data[1];
-}chn_data_t;
+}__attribute__((packed));
+typedef struct chn_data_st chn_data_t;
 
 #endif
 
